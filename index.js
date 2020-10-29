132e6047d764c0f3dfa12627c2ca7782dd7cedcf
Ivona Ze <zelinkova.ivona@gmail.com>
2020-10-29 23:00:16 +0100

    funguje

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ const game = (event) => {
     document.querySelector('.hrac').src = 'images/circle.svg';
     praveHraje = 'circle';
   }
+  if (isWinningMove(event.target)) {
+    //Pokud vrátí is Winning true, zobraz alert s hláškou, který hráč vyhrál.
+    const vyhra = confirm(
+      `Výhra patří${getSymbol(event.target) === 'circle' ? ' kolečku' : ' křížku'
+      }`,
+    );
+    if (vyhra === true) {
+      return window.location.reload(); //aktualizace stranky
+    }
+  }
 };
 
 const field = document.querySelectorAll('.fieldButton'); //vyber tlacitko do promenne field
@@ -23,9 +33,12 @@ for (let i = 0; i < field.length; i++) {
   //cyklus
   field[i].addEventListener('click', game); // na kliknuti zavolej f-ci game
 }
+//       5/5
 
 const boardSize = 10; // 10x10
 const fields = document.querySelectorAll('.fieldButton');
+
+// getPosition(field), která pro dané políčko vrátí objekt s číslem řádku a sloupce
 
 const getPosition = (field) => {
   let fieldIndex = 0;
@@ -42,7 +55,10 @@ const getPosition = (field) => {
   };
 };
 
+// getField(row, column), která pro číslo řádku a sloupce vrátí příslušný prvek
 const getField = (row, column) => fields[row * boardSize + column];
+
+// getSymbol(field), která pro políčko s křížkem vrátí řetězec 'cross', pro kroužek 'circle' a pro neobsazené políčko hodnotu undefined.
 
 const getSymbol = (field) => {
   if (field.classList.contains('board__field--cross')) {
@@ -52,6 +68,8 @@ const getSymbol = (field) => {
   }
 };
 
+//isWinningMove(field), která se podívá na symbol políčka a zjistí, jestli jich je v řádku nebo ve sloupci sousedících pět. Podle toho vrátí true nebo false.
+
 const symbolsToWin = 5;
 const isWinningMove = (field) => {
   const origin = getPosition(field);
@@ -60,14 +78,14 @@ const isWinningMove = (field) => {
   let i;
 
   let inRow = 1; // Jednička pro právě vybrané políčko
-  // Podivej se doleva
+  // Koukni doleva
   i = origin.column;
   while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
     inRow++;
     i--;
   }
 
-  // Podivej se doprava
+  // Koukni doprava
   i = origin.column;
   while (
     i < boardSize - 1 &&
@@ -82,15 +100,15 @@ const isWinningMove = (field) => {
   }
 
   let inColumn = 1;
-  // Podivej se nahoru
-  i = origin.column;
+  // Koukni nahoru
+  i = origin.row;
   while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
     inColumn++;
     i--;
   }
 
-  // Podivej se dolu
-  i = origin.column;
+  // Koukni doprava
+  i = origin.row;
   while (
     i < boardSize - 1 &&
     symbol === getSymbol(getField(i + 1, origin.column))
@@ -104,24 +122,4 @@ const isWinningMove = (field) => {
   }
 
   return false;
-};
-
-//Funkci isWinningMove pusť s každým nově přidaným symbolem.
-
-for (let i = 0; i < field.length; i++) {
-  field[i].addEventListener('click', (event) => {
-    const isWinner = isWinningMove(event.target);
-
-    
-  });
-}
-
-//Pokud vrátí true, zobraz alert s hláškou, který hráč vyhrál.
-
-const vyhra = (event) => {
-  if (isWinningMove === true) {
-    alert('vyhral krizek');
-  } else {
-    alert('vyhralo kolecko');
-  }
 };
